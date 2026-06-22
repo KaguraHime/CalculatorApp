5c956c83d68dd19f6e4f29c05b3a2215a95fcea6
KaguraHime <66565389+KaguraHime@users.noreply.github.com>
2026-06-22 20:40:06 +0800

    Add files via upload

---
+++ CalculatorApp/script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerHTML = "Answer = " + (a + b);
+}
